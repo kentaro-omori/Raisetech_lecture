43b3fcc4291f8c833761a6a03b594328c4711014
kentaro-omori <moriken85@gmail.com>
2022-05-27 05:26:01 +0000

    first commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "a827b1a61956412f8868ab5395ada09d.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
