867d9e6d2c9b5d3add09dc483db1f84eb29e8a0c
kentaro-omori <moriken85@gmail.com>
2022-09-02 10:46:12 +0900

    change serverspec setting

--- serverspec/spec/aws_ec2/sample_spec.rb
+++ serverspec/spec/aws_ec2/sample_spec.rb
@@ -31,7 +31,3 @@ end
 describe port(3306) do
   it { should be_listening }
 end
-
-describe command('curl http://127.0.0.1:#{listen_port}/_plugin/head/ -o /dev/null -w "%{http_code}\n" -s') do
-  its(:stdout) { should match /^200$/ }
-end
