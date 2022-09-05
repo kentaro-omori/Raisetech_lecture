dec849da42e032890f876f936fb8e7d15bed9f5d
kentaro-omori <moriken85@gmail.com>
2022-09-05 17:23:47 +0900

    refactoring

--- spec/ec2_server/sample_spec.rb
+++ spec/ec2_server/sample_spec.rb
@@ -2,10 +2,9 @@ require 'spec_helper'
 
 listen_port = 80
 
-# describe package('rails') do
-# #  it { should be_installed.with_version('2.6.3') }
-#   it { should be_installed.by('gem').with_version('6.1.3.1') }
-# end
+describe port(listen_port) do
+  it { should be_listening }
+end
 
 describe package('nginx') do
   it { should be_installed }
@@ -14,14 +13,6 @@ end
 describe service('nginx') do
   it { should be_enabled }
   it { should be_running }
-end
-
-# describe package('unicorn') do
-#   it { should be_installed.by('gem') }
-# end
-
-describe port(listen_port) do
-  it { should be_listening }
 end
 
 describe service('mysqld') do
