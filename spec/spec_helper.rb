a2643af2b93c0e851baa0ce522455d2173622acf
kentaro-omori <moriken85@gmail.com>
2022-09-05 14:27:47 +0900

    change host name

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,7 @@ host = ENV['TARGET_HOST']
 options = Net::SSH::Config.for(host)
 
 # options[:user] ||= Etc.getlogin
-options[:user] ||= "Raisetech"
+options[:user] ||= "ec2-user"
 
 set :host,        options[:host_name] || host
 set :ssh_options, options
